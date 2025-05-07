322c572869cb3834d4bf2adba1a0faba8b7cd053
Joe <joe@platform48.com>
2025-05-07 18:00:28 +0100

    Added jellyfaas formula - added test

--- Formula/j/jellyfaas.rb
+++ Formula/j/jellyfaas.rb
@@ -2,7 +2,7 @@ class Jellyfaas < Formula
   desc "JellyfaaS CLI"
   homepage "https://github.com/Platform48/jellyfaas_cli"
   url "https://github.com/Platform48/jellyfaas_cli/releases/download/v1.0.0/jellyfaas-v1.0.0-macos.tar.gz"
-  sha256 "ad3c3a782eb4a6b80ebdba7bd71474f7d7c83f559bb997e91a10b0c979a7de90"
+  sha256 "788cce5afd6e52fefa0bcdf537bb95372d32941b4d7bd53239f09d38bb55865f"
   license "MIT"
 
   def install
@@ -10,7 +10,7 @@ class Jellyfaas < Formula
   end
 
   test do
-    system "#{bin}/jellyfaas", "--version"
+    system "#{bin}/jellyfaas", "version"
   end
 end
 
